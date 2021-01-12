8c5f739d5326a281508902a200c9368ce2485306
RafaelPena31 <rafaelppena31@gmail.com>
2021-01-11 23:05:03 -0300

    I Am Poor App

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Rafael Mesquita on 11/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
